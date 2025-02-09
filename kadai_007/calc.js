422bb66ad5f37bf798d1c53a02e5dc2b7001cab8
ZZZoeZZZZ <krt.emp6kkd@gmail.com>
2025-02-09 11:03:12 +0900

    課題7

---
+++ kadai_007/calc.js
@@ -0,0 +1,15 @@
+/* =========== 定数 =========== */
+let num = Math.floor(Math.random()*100);
+
+
+/* =========== 処理 =========== */
+
+if (num % 15 === 0) {
+    console.log('3と5の倍数です');
+} else if (num % 5 === 0) {
+    console.log('5の倍数です');
+} else if (num % 3 === 0) {
+    console.log('3の倍数です');
+} else {
+    console.log(num);
+}
